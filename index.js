05b668d380892dc516a843e7bdd19c21e453b979
mark <markpackham1@gmail.com>
2022-11-15 11:13:06 +0000

    Get all the text off a page
    https://youtu.be/S67gyqnYHmI?t=558

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ async function run() {
   const page = await browser.newPage();
   await page.goto("https://www.traversymedia.com");
 
-  await page.screenshot({ path: "example.png" });
+  // This site page has less to scrape so saves time
+  // await page.goto("https://www.ed.ac.uk/about/our-history");
+
+  // This site page has less to scrape so saves time
+  // await page.goto("https://www.google.com/");
+
+  //  screenshot of the page
+  // await page.screenshot({ path: 'example.png', fullPage: true });
+
+  //  PDF of the page
+  // await page.pdf({ path: "example.pdf", format: "A4" });
+
+  // const html = await page.content();
+  // console.log(html);
+
+  // const title = await page.evaluate(() => document.title);
+  // console.log(title);
+
+  // text of the page
+  const text = await page.evaluate(() => document.body.innerText);
+  console.log(text);
 
   await browser.close();
 }
